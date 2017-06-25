1e65681d943c86734f9fb4f50c6be6c859ae5c48
Aaron F <aaron@aaronf.com>
2017-06-25 12:10:38 -0400

    changes

--- src/net.cpp
+++ src/net.cpp
@@ -1061,7 +1061,7 @@ void ThreadMapPort2(void* parg)
             }
         }
 
-        string strDesc = "SmallChange " + FormatFullVersion();
+        string strDesc = "DoraCoin " + FormatFullVersion();
 #ifndef UPNPDISCOVER_SUCCESS
         /* miniupnpc 1.5 */
         r = UPNP_AddPortMapping(urls.controlURL, data.first.servicetype,
@@ -1142,7 +1142,7 @@ void MapPort()
 
 
 
-
+//** need to modify and add seeds **//
 
 // DNS seeds
 // Each pair gives a source name and a seed name.
@@ -1150,8 +1150,8 @@ void MapPort()
 // The second name should resolve to a list of seed addresses.
 static const char *strDNSSeed[][2] = {
     //{"litecoinpool.org", "dnsseed.litecoinpool.org"},
-    //{"bytesized-vps.com", "dnsseed.bytesized-vps.com"},
-    //{"xurious.com", "dnsseed.ltc.xurious.com"},
+    {"francesconi.org", "dnsseed.francesconi.org"},
+    {"aaronf.com", "dnsseed.aaronf.com"},
 };
 
 void ThreadDNSAddressSeed(void* parg)
@@ -1727,7 +1727,7 @@ bool BindListenPort(const CService &addrBind, string& strError)
     {
         int nErr = WSAGetLastError();
         if (nErr == WSAEADDRINUSE)
-            strError = strprintf(_("Unable to bind to %s on this computer. SmallChange is probably already running."), addrBind.ToString().c_str());
+            strError = strprintf(_("Unable to bind to %s on this computer. DoraCoin is probably already running."), addrBind.ToString().c_str());
         else
             strError = strprintf(_("Unable to bind to %s on this computer (bind returned error %d, %s)"), addrBind.ToString().c_str(), nErr, strerror(nErr));
         printf("%s\n", strError.c_str());
